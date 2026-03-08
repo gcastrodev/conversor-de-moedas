964cdb4cac54ce8fde7fef37f968d26db76094d1
gcastrodev <castrovisionario@icloud.com>
2026-03-08 00:43:22 -0300

    Ajusta conversão para Euro

--- main.js
+++ main.js
@@ -1,37 +1,52 @@
-const convertButton = document.querySelector('.convertButton');
-const currencyInput = document.querySelector('.currencyInput');
+const convertButton = document.querySelector(".convertButton");
+const currencySelect = document.querySelector(".currencySelect");
+const currencyInput = document.querySelector(".currencyInput");
 
-const currencyValues = document.querySelectorAll('.currency-value');
+const currencyValueToConvert = document.querySelector(
+  ".currency-value-to-convert",
+);
+const currencyValue = document.querySelector(".currency-value");
 
-function convertMoney() {
-    const inputValue = currencyInput.value;
+const currencyName = document.querySelector(".currencyName");
+const currencyFlag = document.querySelector(".currencyFlag");
 
-    const url = "https://v6.exchangerate-api.com/v6/37ce2f7504711a40d2c7fda9/latest/USD";
+async function convertMoney() {
+  const inputValue = currencyInput.value;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
+  const url =
+    "https://v6.exchangerate-api.com/v6/37ce2f7504711a40d2c7fda9/latest/BRL";
 
-            const dollarRate = data.conversion_rates.BRL;
+  const response = await fetch(url);
+  const data = await response.json();
 
-            const convertedValue = inputValue / dollarRate;
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputValue);
 
-            // Valor em Real (lado esquerdo)
-            currencyValues[0].innerHTML = new Intl.NumberFormat('pt-BR', {
-                style: 'currency',
-                currency: 'BRL'
-            }).format(inputValue);
+  const rate = data.conversion_rates[currencySelect.value];
 
-            // Valor em Dólar (lado direito)
-            currencyValues[1].innerHTML = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD'
-            }).format(convertedValue);
+  const convertedValue = inputValue * rate;
 
-        })
-        .catch(error => {
-            console.error('Erro ao obter a taxa de câmbio:', error);
-        });
+  currencyValue.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currencySelect.value,
+  }).format(convertedValue);
 }
 
-convertButton.addEventListener('click', convertMoney);
+function changeCurrency() {
+  if (currencySelect.value === "USD") {
+    currencyName.innerHTML = "Dólar Americano";
+    currencyFlag.src = "assets/usa-flag.svg";
+  }
+
+  if (currencySelect.value === "EUR") {
+    currencyName.innerHTML = "Euro";
+    currencyFlag.src = "assets/euro.png";
+  }
+
+  convertMoney();
+}
+
+currencySelect.addEventListener("change", changeCurrency);
+convertButton.addEventListener("click", convertMoney);
